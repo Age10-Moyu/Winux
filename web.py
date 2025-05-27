31cca5c3b911b86d035e88c0df510a102f718227
Age10_Moyu <yx_zyz20120418@163.com>
2025-05-27 13:40:53 +0800

    Delete web.py

--- web.py
+++
@@ -1,5 +0,0 @@
-import subprocess
- 
-def connect(show=True):
-        ret = subprocess.run("ping 110.242.68.66 -n 1", shell=show, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return True if ret.returncode == 200 else False
